6ddf4022272389691aa1cbf8eb272e83802df474
davidmss2000 <davidmss2000>
2019-12-09 11:53:55 +0100

    password hashing

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,9 +1,14 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/user');
+
+// Password hashing
+const saltRounds = 10;
 
 function signup(req, res) {
   if (!req.body['password']) return res.status(400).send({ error: 'Password needed' });
 
-  const passwordLength = req.body['password'].length;
+  const passwordLength = req.body.password.length;
   if (passwordLength < 6) {
     return res.status(400).send({ error: 'Password is too short, it must be between 6 and 50 characters' });
   }
@@ -11,30 +16,50 @@ function signup(req, res) {
     return res.status(400).send({ error: 'Password is too long, it must be between 6 and 50 characters' });
   }
   
-  // Save User in the database
   const user = new User(req.body);
-  user.save((err, obj) => {
-    if (err) return res.status(400).send({ error: err.message });
+  // Hash the password
+  bcrypt.hash(req.body.password, saltRounds, (err, hashed) => {
+    if (err){
+      console.log(err);
+      return res.status(500).send({ error: 'There was an error processing your request' });
+    }
     
+    user.password = hashed;
+
+    // Save User
+    user.save((err, obj) => {
+    if (err) return res.status(400).send({ error: err.message });
     return res.status(201).send({ obj });
+    });
   });
 }
 
 function login(req, res) {
   const { password } = req.body;
   const { email } = req.body;
+  const { username } = req.body;
 
-  User.findOne({ email }, (err, user) => {
+  let field;
+  // If username exists then field = { "username":username } else field = { "email":email }
+  username ? field = { "username":username } : field = { "email":email };
+
+  User.findOne(field, (err, user) => {
     if (err) {
       console.log(err);
       return res.status(500).send({ error: 'There was an error processing your request' });
     }
-    if (!user) return res.status(404).send({ error: 'Email not found' });
+    if (!user) return res.status(404).send({ error: 'User not found' });
     
-    // TODO compare using bcrypt
-    if (password != user.password) return res.status(404).send({ error: 'Wrong password' });
-    
-    return res.status(200).send({ user });
+    // Compare using bcrypt
+    bcrypt.compare(password, user.password, (err, same) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send({ error: 'There was an error processing your request' });
+      }
+      if (!same) return res.status(404).send({ error: 'Wrong password' });
+      // Correcto
+      return res.status(200).send({ user });
+    })
   });
 }
 
