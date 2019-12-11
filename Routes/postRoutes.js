cee7bc2926378095f4fad9d10aeca58889bc5a45
davidmss2000 <davidmss2000>
2019-12-11 11:12:34 +0100

    more minor fixes

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -3,16 +3,16 @@ const router = require('express').Router();
 const postRoutes = require('../Controllers/postController');
 
 // Create a new post
-router.post('/p/new', postRoutes.createPost);
+router.post('/new', postRoutes.createPost);
 // Returns post object
-router.get('/p/:postId', postRoutes.getPost);
+router.get('/:postId', postRoutes.getPost);
 // Returns user posts
 router.get('/user/:username', postRoutes.getUserPosts);
 // Returns topic posts
 router.get('/topic/:topicId', postRoutes.getTopicPosts);
 // Deletes post
-router.delete('/p/:postId/delete', postRoutes.deletePost);
+router.delete('/delete/:postId', postRoutes.deletePost);
 // Returns post object
-router.patch('/p/:postId/edit', postRoutes.patchPost);
+router.patch('/edit/:postId', postRoutes.patchPost);
 
 module.exports = router;
