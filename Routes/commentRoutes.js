855d70729530dbc04868b2d5798f05c3a6653ef0
davidmss2000 <davidmss2000>
2019-12-11 11:35:17 +0100

    comment routes and controller

---
+++ Routes/commentRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const commentController = require('../Controllers/commentController');
+
+// Create a new comment
+router.post('/new', commentController.createComment);
+// Returns comment object
+router.get('/:commentId', commentController.getComment);
+// Returns user comments
+router.get('/user/:username', commentController.getUserComments);
+// Returns post comments
+router.get('/post/:postId', commentController.getPostComments);
+// Deletes comment
+router.delete('/delete/:commentId', commentController.deleteComment);
+// Returns comment object
+router.patch('/edit/:commentId', commentController.patchComment);
+
+module.exports = router;
