2de2c5f32b41e94fa48130fd3f7bc341d5c0e3b3
Merk <thezyndar@gmail.com>
2019-12-12 00:32:05 +0100

    Update pmController.js

--- Controllers/pmController.js
+++ Controllers/pmController.js
@@ -29,20 +29,6 @@ function getUserPMs(req, res) {
   });
 }
 
-function getPMsFromUser(req, res) {
-  const senderUsername = { senderUsername: req.params.senderUsername };
-
-  PM.find(senderUsername, (err, pm) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send({ error: 'There was an error processing your request' });
-    }
-    if (!pm) return res.status(404).send({ error: 'No PMs from this user.' });
-    return res.status(200).send({ pm });
-  });
-}
-
-
 module.exports = {
   sendPM,
   getUserPMs,
