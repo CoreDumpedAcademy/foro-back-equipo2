857f73bd266e57db9e64c3d531d54dcbd2b67171
unknown <albertoleonsanchez@gmail.com>
2019-12-11 10:13:53 +0100

    access from front to the API

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Parse requests of content-type - application/json
 app.use(bodyParser.json());
 
+//allow access to the API
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Alow-Methods', 'PUT,GET,POST,OPTIONS');
+  next();
+});
+
 // Set /user as base url for user
 app.use('/user', user);
 app.use('/post', postRoutes);
