444b93923770650025023b1555421f2226a952dd
unknown <albertoleonsanchez@gmail.com>
2019-12-12 21:04:27 +0100

    description in Topic model

--- Models/topicModel.js
+++ Models/topicModel.js
@@ -5,6 +5,10 @@ const TopicSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  description: {
+    type: String,
+    require: true,
+  },
   title: {
     type: String,
     required: true,
