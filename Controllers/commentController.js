d6c067948016c776e28e94524ec7ab8f3e433bb2
Merk <thezyndar@gmail.com>
2019-12-18 18:25:17 +0100

    Added Pagination to Posts and Topics

--- Controllers/commentController.js
+++ Controllers/commentController.js
@@ -26,8 +26,16 @@ function getComment(req, res) {
 
 function getUserComments(req, res) {
   const { usernameId } = req.params;
+  var pageNo = parseInt(req.query.pageNo);
+  var size = parseInt(req.query.size);
+  var query = {};
 
-  Comment.find({ 'usernameId':usernameId }, (err, comments) => {
+  if (pageNo < 0 || pageNo === 0) return res.send({ err: "Invalid page number" }); 
+
+  query.skip = size * (pageNo - 1);
+  query.limit = size;
+
+  Comment.find({ 'usernameId':usernameId }, {}, query, (err, comments) => {
     if (err) {
       console.log(err);
       return res.status(500).send({ error: 'There was an error processing your request' });
