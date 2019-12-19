fc142a4f9f9ddcdff9c72b93a1964ae555df2e34
unknown <albertoleonsanchez@gmail.com>
2019-12-19 01:43:34 +0100

    Insta message progress

---
+++ Controllers/instaMessageControler.js
@@ -0,0 +1,33 @@
+const InstaMessage = require('../Models/instaMessageModel');
+
+function saveInstaMessage(req, res) {
+    if (!req.body.receiverUsernameId) return res.status(400).send({ error: 'Recipient needed' });
+    if (!req.body.usernameId) return res.status(400).send({ error: 'Title needed' });
+    if (!req.body.content) return res.status(400).send({ error: 'Content needed' });
+  
+    const instaMessage = new InstaMessage(req.body);
+  
+    instaMessage.save((err, obj) => {
+      if (err) return res.status(400).send({ error: err.message });
+      return res.status(201).send({ obj });
+    });
+  }
+  
+  function getUserInstantMessages(req, res) {
+    const currentUser =  req.params.usernameId;
+  
+    InstaMessage.find({ 'receiverUsernameId': currentUser }, (err, instaMessage) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send({ error: 'There was an error processing your request' });
+      }
+      if (!instaMessage) return res.status(404).send({ error: 'This user has no InstaMessages.' });
+      return res.status(200).send({ instaMessage });
+    });
+  }
+
+
+module.exports = {
+    saveInstaMessage,
+    getUserInstantMessages,
+};
